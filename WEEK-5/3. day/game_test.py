e49d01ad22638d758bf873545a47b1eedd21741f
György Csadó <gyorgy.csado@gmail.com>
2015-12-09 17:45:34 +0100

    functional programing practice

---
+++ WEEK-5/3. day/game_test.py
@@ -0,0 +1,26 @@
+import unittest
+import game
+
+class TestFunc(unittest.TestCase):
+    def setUp(self):
+        self._c1 = game.Character(health=10,armor=5)
+        self._c2 = game.Character(50,30)
+
+        self._eventList = [
+        {'character': self._c2,'type':'damage','size':50},
+        {'character': self._c1,'type':'damage','size':50},
+        {'character': self._c1,'type':'heal','size':50}
+        ]
+
+
+    def test_should_handle_the_damage_events(self):
+        game.handleEvents(self._eventList)
+        self.assertFalse(self._c1.isAlive())
+        self.assertTrue(self._c2.isAlive())
+
+    def test_should_heal_a_character(self):
+        game.handleEvents(self._eventList)
+        self.assertEqual(self._c1.getHealth(),60)
+
+
+unittest.main()
