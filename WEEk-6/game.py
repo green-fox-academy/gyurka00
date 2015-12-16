f0c9d8c9e7e8e665ef4233f89d94773d620bf009
György Csadó <gyorgy.csado@gmail.com>
2015-12-16 15:57:56 +0100

    fire alarm

--- WEEk-6/game.py
+++ WEEk-6/game.py
@@ -1,46 +1,62 @@
 import menu
 from menu_item import MenuItem
 import player
+import json
 
 user = player.Player()
 
 def save():
-    print('save')
+    data_file = open('data.json', 'w')
+    json.dump(user.create_dictionary(), data_file)
+    data_file.close()
 
-def reenter_name():
-    return new_game()
+def strike():
+    pass
 
-def reroll_stats():
-    return new_game_continue()
+def retreat():
+    pass
 
-def stats_quite():
-    print('stats_quite')
+def begin_quite():
+    pass
+
+def begin_menu():
+    par = [
+        MenuItem(1, 'Strike', strike),
+        MenuItem(2, 'Retreat', retreat),
+        MenuItem(3, 'Quit', begin_quite)
+        ]
+    potion_continue = menu.Menu(par)
+    potion_continue.menu_draw()
 
 def begin():
-    pass
-
-def potion_continue_save():
-    pass
+    print('\n---Test Fight---\n')
+    print('User')
+    print(user.print_all_stats())
+    monster = player.Player()
+    print('Monster')
+    print(monster.print_monster_stats())
+    begin_menu()
 
 def potion_continue_quite():
-    pass
+    return stats_continue()
 
 def potion_continue():
     print(user.print_all_stats())
     par = [
         MenuItem(1, 'Begin', begin),
-        MenuItem(2, 'Save', potion_continue_save),
+        MenuItem(2, 'Save', save),
         MenuItem(3, 'Quit', potion_continue_quite)
         ]
     potion_continue = menu.Menu(par)
     potion_continue.menu_draw()
-    Begin, Save, Quit
 
 def potion_quite():
-    print('potion_quite')
+    return stats_continue()
+
 
 def potion_selector(potion):
     user.insert_potion(potion)
+    print(user.inventory[0])
     par = [
         MenuItem(1, 'Reselect the Potion', stats_continue),
         MenuItem(2, 'Continue', potion_continue),
@@ -49,12 +65,14 @@ def potion_selector(potion):
     selected_potion = menu.Menu(par)
     selected_potion.menu_draw()
 
+
+
 def stats_continue():
     par = [
-        MenuItem(1, 'Potion of Health', potion_selector),
-        MenuItem(2, 'Potion of Dexterity', potion_selector),
-        MenuItem(3, 'Potion of Luck', potion_selector)
-        ]
+    MenuItem(1, 'Potion of Health', potion_selector),
+    MenuItem(2, 'Potion of Dexterity', potion_selector),
+    MenuItem(3, 'Potion of Luck', potion_selector)
+    ]
     potion = menu.Menu(par)
     print(potion.menu_list())
     selected_potion = potion.user_input()
@@ -62,6 +80,11 @@ def stats_continue():
         if item.num == selected_potion:
             return item.func(item.name)
 
+def reroll_stats():
+    return new_game_continue()
+
+def quite_to_new_game():
+    return new_game()
 
 def new_game_continue():
     user.random_stats()
@@ -70,22 +93,23 @@ def new_game_continue():
         MenuItem(1, 'Reroll stats', reroll_stats),
         MenuItem(2, 'Continue', stats_continue),
         MenuItem(3, 'Save', save),
-        MenuItem(4, 'Quit', stats_quite)
+        MenuItem(4, 'Quit', quite_to_new_game)
         ]
     quit_new_game = menu.Menu(par)
     quit_new_game.menu_draw()
 
 
 def save_and_quite():
-    print('save_and_quite')
+    save()
+    return main()
 
 def quite_without_save():
-    pass
+    return main()
 
 def resume():
-    pass
+    return new_game_continue()
 
-def quit():
+def quit_to_main():
     par = [
         MenuItem(1, 'Save and Quit', save_and_quite),
         MenuItem(2, 'Quit without save', quite_without_save),
@@ -93,6 +117,9 @@ def quit():
         ]
     quit_new_game = menu.Menu(par)
     quit_new_game.menu_draw()
+
+def reenter_name():
+    return new_game()
 
 def new_game():
     user_name = input('Given a user name: ')
@@ -102,21 +129,24 @@ def new_game():
         MenuItem(1, 'Reenter name', reenter_name),
         MenuItem(2, 'Continue', new_game_continue),
         MenuItem(3, 'Save', save),
-        MenuItem(4, 'Quit', quit)
+        MenuItem(4, 'Quit', quit_to_main)
         ]
     new_game = menu.Menu(par)
     new_game.menu_draw()
 
 def load_game():
-    print('hutyutyu')
+    print('Load Game')
 
 def exit_game():
     exit()
 
-par = [
-    MenuItem(1, 'New Game', new_game),
-    MenuItem(2, 'Load Game', load_game),
-    MenuItem(3, 'Exit', exit)
-    ]
-main_menu = menu.Menu(par)
-main_menu.menu_draw()
+def main():
+    par = [
+        MenuItem(1, 'New Game', new_game),
+        MenuItem(2, 'Load Game', load_game),
+        MenuItem(3, 'Exit', exit)
+        ]
+    main_menu = menu.Menu(par)
+    main_menu.menu_draw()
+
+main()
--- WEEk-6/player.py
+++ WEEk-6/player.py
@@ -3,26 +3,47 @@ class Player():
     def __init__(self, name = ''):
         self.name = name
         self.random_stats()
-        self.inventory = ['', 'Leather Armor', 'Sword']
+        self.inventory = ['None', 'Leather Armor', 'Sword']
 
     def player_stats(self):
         return ('Dexterity: ' + str(self.dexterity) + ' , Health: ' + str(self.health) + ' , Luck: ' + str(self.luck))
 
     def random_stats(self):
-        self.dexterity = random.randrange(1, 6) + 6
+        self.dexterity = self.roll() + 6
         self.health = random.randrange(1, 6) + random.randrange(1, 6) + 12
         self.luck = random.randrange(1, 6) + 6
+        self.max_dexterity = self.dexterity
+        self.max_health = self.health
+        self.max_luck = self.luck
+
+    @staticmethod
+    def roll():
+        number = random.randrange(1, 6)
+        return number
 
     def insert_potion(self, potion):
         del self.inventory[0]
         self.inventory.insert(0, potion)
 
+    def print_monster_stats(self):
+        output = ''
+        output += 'Max Dexterity: ' + str(self.max_dexterity) + '\n'
+        output += 'Current Dexterity: ' + str(self.dexterity) + '\n'
+        output += 'Max Health: ' + str(self.max_health) + '\n'
+        output += 'Current Health: ' + str(self.health) + '\n'
+        return output
+
     def print_all_stats(self):
         output = ''
-        output += 'Dexterity: ' + str(self.dexterity) + '\n'
-        output += 'Health: ' + str(self.health) + '\n'
-        output += 'Luck: ' + str(self.luck) + '\n'
+        output += 'Namme: ' + str(self.name) + '\n'
+        output += self.print_monster_stats()
+        output += 'Max Luck: ' + str(self.max_luck) + '\n'
+        output += 'Current Luck: ' + str(self.luck) + '\n'
         output += 'Inventory:\n'
         for i in self.inventory:
             output += '\t' + i + '\n'
         return output
+
+    def create_dictionary(self):
+        dictionary = {'name' : self.name, 'dexterity' : self.dexterity, 'health' : self.health, 'luck' : self.luck, 'inventory' : [self.inventory[0], self.inventory[1], self.inventory[2]]}
+        return dictionary
