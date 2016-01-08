7f476ebe821913c1834ed29bb31861c5a2185a64
György Csadó <gyorgy.csado@gmail.com>
2016-01-08 01:13:13 +0100

    stop jpg and png push

--- WEEK-7/3. day/triangle.js
+++ WEEK-7/3. day/triangle.js
@@ -7,4 +7,27 @@ function triangle (n) {
 }
 triangle(3);
 console.log('\n\n');
-triangle(84);
+triangle(4);
+
+//pasca triangle
+
+function pascalTriangle(n) {
+  var pascalArray = [[1]];
+  if (n > 1) {
+    for (var i = 1; i <= n-1; i++) {
+        var line = [];
+        line.push(1);
+        for (var j = 1;j < pascalArray.length; j++) {
+            line.push(pascalArray[i-1][j] + pascalArray[i-1][j-1])
+        }
+        line.push(1);
+        pascalArray.push(line);
+    }
+  } else {console.log('invalid size');}
+  console.log('\n');
+  for (var i = 0; i < n; i++) {
+      console.log(' '.repeat(n - i) + pascalArray[i]);
+  }
+}
+
+pascalTriangle(10);
