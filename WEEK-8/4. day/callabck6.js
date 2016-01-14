211449b40bb5b0caaab225ce6f89e2ba1764d0a2
György Csadó <gyorgy.csado@gmail.com>
2016-01-14 11:47:08 +0100

    callabck practice

---
+++ WEEK-8/4. day/callabck6.js
@@ -0,0 +1,11 @@
+'use strict';
+
+var fs = require('fs')
+
+function countLetterInAlmaTxt(letter,callabck) {
+  //befejezni
+}
+
+countLetterInAlmaTxt('a',function (count) {
+  console.log(count); //->1
+}); //in alma.txt
